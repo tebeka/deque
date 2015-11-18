d0c6f380df9bb5e20cf9460bde2dd191ab229c3f
Miki Tebeka <miki.tebeka@gmail.com>
2015-11-18 20:02:58 +0200

    better? doc

--- doc.go
+++ doc.go
@@ -1,38 +1,38 @@
 /*
 // Example:
 
-import (
-	"fmt"
+	import (
+		"fmt"
 
-	"github.com/tebeka/deque"
-)
+		"github.com/tebeka/deque"
+	)
 
-func Example() {
-	// Create a new deque and put some numbers in it.
-	dq := deque.New()
-	for i := 0; i < 5; i++ {
-		dq.Append(i)
-	}
+	func Example() {
+		// Create a new deque and put some numbers in it.
+		dq := deque.New()
+		for i := 0; i < 5; i++ {
+			dq.Append(i)
+		}
 
-	// Pop from the left
-	val, _ := dq.PopLeft()
-	fmt.Println(val) // 0
+		// Pop from the left
+		val, _ := dq.PopLeft()
+		fmt.Println(val) // 0
 
-	// Get an item
-	val, _ = dq.Get(2)
-	fmt.Println(val) // 2
+		// Get an item
+		val, _ = dq.Get(2)
+		fmt.Println(val) // 2
 
-	// Set an item
-	dq.Set(2, "hello")
+		// Set an item
+		dq.Set(2, "hello")
 
-	// Rotate
-	dq.Rotate(-2)
+		// Rotate
+		dq.Rotate(-2)
 
-	// Print
-	fmt.Println(dq)
+		// Print
+		fmt.Println(dq)
 
-	// Output:
-	// Deque{"hello", 4, 1, 2}
+		// Output:
+		// Deque{"hello", 4, 1, 2}
 }
 */
 package deque
